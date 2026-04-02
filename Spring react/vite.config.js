ad63d2992bc2322ff62b7362a96e483122d0986d
murtuza9270 <murtuza9270@gmail.com>
2026-04-03 00:09:31 +0530

    Delete Spring react folder

--- Spring react/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Try without Tailwind plugin first to test
-export default defineConfig({
-  plugins: [
-    react()
-  ]
-})
